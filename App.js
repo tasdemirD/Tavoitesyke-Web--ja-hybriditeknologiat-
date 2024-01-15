cbafcd8d3b579cc4e443c93d020a44709d8f2f5b
juujoojaa <thiscantbewhy@gmail.com>
2024-01-15 12:13:41 +0200

    Tiedostot gittiin

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [lowerLimit, setLLimit] = useState(0)
+  const [upperLimit, setULimit] = useState(0)
+
+  const calculate = () => {
+    const lowerResult = (220 - age) *  0.65;
+    setLLimit(lowerResult)
+
+    const upperResult = (220 - age) *  0.85;
+    setULimit(upperResult)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Age</Text>
+      <TextInput
+      style={styles.field}
+      placeholder='Enter age'
+      keyboardType='decimal-pad'
+      value={age}
+      onChangeText={text => setAge(text)}
+      />
+
+      <Text>Limits</Text>
+      <Text style={styles.field}>{lowerLimit} - {upperLimit}</Text>
+
+      <Button title='Calculate' onPress={calculate}/>
     </View>
   );
 }
@@ -14,7 +37,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 13,
+    marginRight: 13
   },
+  field: {
+    marginBottom: 10
+  }
 });
